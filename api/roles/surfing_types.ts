e787e0506d63a13d2a47959237ab13174f661244
Rydix <73070463+rydixulous@users.noreply.github.com>
2022-05-30 14:08:57 +0300

    added emojis to surfing types

--- api/roles/surfing_types.ts
+++ api/roles/surfing_types.ts
@@ -1,5 +1,5 @@
 export const surfingTypesDropdown = {
-    content: `Alright! Select your surfing types
+    content: `Alright! Select your **Surfing Types**
 > *Tip: choose **Pronouns** next!*`,
     flags: 64,
     components: [
@@ -15,36 +15,60 @@ export const surfingTypesDropdown = {
                             value: 'longboard',
                             description:
                                 'Add Longboard to your surfing types',
+                            emoji: {
+                                name: 'Longboard',
+                                id: '980786936413298688',
+                            },
                         },
                         {
                             label: 'Shortboard',
                             value: 'shortboard',
                             description:
                                 'Add Shortboard to your surfing types',
+                            emoji: {
+                                name: 'Shortboard',
+                                id: '980786936597852180',
+                            },
                         },
                         {
                             label: 'Skimboard',
                             value: 'skimboard',
                             description:
                                 'Add Skimboard to your surfing types',
+                            emoji: {
+                                name: 'Skimboard',
+                                id: '980786936555900948',
+                            },
                         },
                         {
                             label: 'Wakeboard',
                             value: 'wakeboard',
                             description:
                                 'Add Wakeboard to your surfing types',
+                            emoji: {
+                                name: 'Wakeboard',
+                                id: '980786936987930664',
+                            },
                         },
                         {
                             label: 'Kiteboard',
                             value: 'kiteboard',
                             description:
                                 'Add Kiteboard to your surfing types',
+                            emoji: {
+                                name: 'Kiteboard',
+                                id: '980786935494742026',
+                            },
                         },
                         {
                             label: 'Windboard',
                             value: 'windboard',
                             description:
                                 'Add Windboard to your surfing types',
+                            emoji: {
+                                name: 'Windboard',
+                                id: '980786936887255040',
+                            },
                         },
                     ],
                     placeholder: 'Choose your surfing types',
